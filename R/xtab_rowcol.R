e11f2c01ea0c2f5346f10afd46913716eeb90bb7
Margaret Hannum <margie.hannum@gmail.com>
2019-08-14 11:23:11 -0400

    add new crosstab function

---
+++ R/xtab_rowcol.R
@@ -0,0 +1,41 @@
+#' Calculate row AND column crosstabulatd percent
+#'
+#' Useful in comparing cluster assignments
+#'
+#' @param x crosstabulated matrix (e.g. cluster assignments)
+#' @param rowvar name of row variables
+#' @param colvar name of column variables
+#'
+#' @export
+#' @author Margaret L. Hannum
+#' @examples
+#
+# x <- cbind( x1 = c(1, 0, 0, 4), x2 = c(0, 1, 5, 0), x3 = c(6, 3, 0, 0))
+# xtab_rowcol(x, "a", "b")
+
+xtab_rowcol <- function(x, rowvar, colvar) {
+  x1 <- matrix(rep(rowSums(x, na.rm = TRUE), ncol(x)), ncol = ncol(x))
+  x2 <- matrix(rep(colSums(x, na.rm = TRUE), nrow(x)), nrow = nrow(x), byrow = TRUE)
+  x3 <- x1 + x2
+  x4 <- x/x3
+  colname <- dplyr::enquo(colvar)
+
+  df <- tibble::rownames_to_column(tibble::as_tibble(x4), rowvar)
+
+  long <-
+    df %>%
+    tidyr::gather((!!colname), percent, -rowvar)
+
+  long2 <- long %>%
+    dplyr::arrange(dplyr::desc(percent)) %>%
+    dplyr::mutate(new_var = dplyr::row_number())
+
+  long3 <- long2 %>%
+    dplyr::distinct(nebula, .keep_all = TRUE)
+#
+#   x_new <- x[as.numeric(long3[[rowvar]]), long3[[colvar]]]
+#   return(x_new)
+}
+
+# x <- cbind( x1 = c(1, 0, 0, 4), x2 = c(0, 1, 5, 0), x3 = c(6, 3, 0, 0))
+# t <- xtab_rowcol(x, "nebula", "icluster")
