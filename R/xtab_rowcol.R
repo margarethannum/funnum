397dfb73c7cef102a9a9b067374a3d54a155b784
Margaret Hannum <margie.hannum@gmail.com>
2019-11-12 12:31:24 -0500

    Update xtab_rowcol.R

--- R/xtab_rowcol.R
+++ R/xtab_rowcol.R
@@ -19,6 +19,7 @@ xtab_rowcol <- function(x, rowvar, colvar) {
   x3 <- x1 + x2
   x4 <- x/x3
   colname <- dplyr::enquo(colvar)
+  rowname <- dplyr::enquo(rowvar)
 
   df <- tibble::rownames_to_column(tibble::as_tibble(x4), rowvar)
 
@@ -31,11 +32,35 @@ xtab_rowcol <- function(x, rowvar, colvar) {
     dplyr::mutate(new_var = dplyr::row_number())
 
   long3 <- long2 %>%
-    dplyr::distinct(nebula, .keep_all = TRUE)
+    dplyr::distinct_(rowvar, colvar, .keep_all = TRUE)
 #
 #   x_new <- x[as.numeric(long3[[rowvar]]), long3[[colvar]]]
 #   return(x_new)
 }
 
 # x <- cbind( x1 = c(1, 0, 0, 4), x2 = c(0, 1, 5, 0), x3 = c(6, 3, 0, 0))
-# t <- xtab_rowcol(x, "nebula", "icluster")
+t <- xtab_rowcol(x, "nebula", "icluster")
+
+
+xtab_rowcol <- function(x, rowvar, colvar) {
+  x1 <- matrix(rep(rowSums(x, na.rm = TRUE), ncol(x)), ncol = ncol(x))
+  x2 <- matrix(rep(colSums(x, na.rm = TRUE), nrow(x)), nrow = nrow(x), byrow = TRUE)
+  x3 <- x1 + x2
+  x4 <- x/x3
+
+  df <- tibble::rownames_to_column(tibble::as_tibble(x4), !!rlang::sym(rowvar))
+  #
+  # long <-
+  #   df %>%
+  #   tidyr::gather(!!colvar, percent, -!!rowvar)
+  #
+  # long2 <- long %>%
+  #   dplyr::arrange(dplyr::desc(percent)) %>%
+  #   dplyr::mutate(new_var = dplyr::row_number())
+  #
+  # long3 <- long2 %>%
+  #   dplyr::distinct(!!rowvar, .keep_all = TRUE)
+  #
+  #   x_new <- x[as.numeric(long3[[rowvar]]), long3[[colvar]]]
+  #   return(x_new)
+}
